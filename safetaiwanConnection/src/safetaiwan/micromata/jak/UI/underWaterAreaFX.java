cb8561359a3f6087ff06e31317322b282058a240
c2publictemp <c2publictemp@gmail.com>
2016-08-09 10:32:15 +0800

    UIFX

--- safetaiwanConnection/src/safetaiwan/micromata/jak/UI/underWaterAreaFX.java
+++ safetaiwanConnection/src/safetaiwan/micromata/jak/UI/underWaterAreaFX.java
@@ -1,10 +1,29 @@
 package safetaiwan.micromata.jak.UI;
 
-public class underWaterAreaFX {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class underWaterAreaFX extends Application {
+	private Stage stage;
+	private final double MINIMUM_WINDOW_WIDTH = 750;
+	private final double MINIMUM_WINDOW_HEIGHT = 650;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Application.launch(underWaterAreaFX.class, (java.lang.String[]) null);
 	}
+	public void start(Stage primaryStage) {
+		try {
+			this.stage = primaryStage;
+			this.stage.setMinWidth(MINIMUM_WINDOW_WIDTH);
+			this.stage.setMinHeight(MINIMUM_WINDOW_HEIGHT);
+			this.stage.setTitle("SafeTaiwan-UnderWater");
 
+			primaryStage.show();
+		} catch (Exception ex) {
+			Logger.getLogger(underWaterAreaFX.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
